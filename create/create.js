a4e8b806b896e1b25fcedc50fe348a20439b21e7
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-05-24 12:05:15 -0700

    everything working

--- create/create.js
+++ create/create.js
@@ -13,3 +13,13 @@ async function onLoad() {
 }
 onLoad();
 
+participantForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const form = new FormData(participantForm);
+    await createParticipant({
+        name: form.get('name'),
+        contact_info: form.get('contact'),
+        workshop_id: form.get('workshop_id'),
+    });
+    window.location.href = '/workshops';
+});
